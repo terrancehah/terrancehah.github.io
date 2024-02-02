2266d6baa53bfee177ea30ceaa1cd85e74c24895
Terrance <terrancehah@gmail.com>
2024-02-02 16:06:10 +0800

    optimisation of pdf header

--- api/travel-rizz-conclusion.js
+++ api/travel-rizz-conclusion.js
@@ -30,7 +30,7 @@ module.exports = async (req, res) => {
                     First, make a page/div for the travel reminders, separate content into Travel Document and Visa Requirement, Tax Refund Procedures, Latest Current Issues and Advisories, Local Etiquette and Cultural Norms, and Health and Vaccination Record Requirement.
                     Then, do one page/div for Emergency Contacts. Provide emergency contact numbers, including local police, ambulance, and fire department and so on. Then, provide 2-4 hospitals in the city along with their contacts and address.
                     In the same div/page, construct messages under the emergency contacts to remind users to be aware of the location and contact information of their respective embassies in the city. Attach the link https://www.embassy-worldwide.com/ in your message.
-                    Lastly, Do one last page/div for the Farewell Messages, wishing travelers a wonderful journey and inviting them to share their experiences or feedback.
+                    Lastly, Do one last page/div for the Farewell Messages consisting about 2-3 sentences, wishing travelers a wonderful journey and inviting them to share their experiences or feedback. Do not prompt travellers to call us, as we are only a travel itinerary helper, not a travel agency.
                     
                     Response Format:
                     Since the response here will be redirected to be shown on a HTML page, you must format each section with HTML tags.
@@ -42,7 +42,7 @@ module.exports = async (req, res) => {
                     Do not use <ul></ul> or <li></li> tags, as they do get misaligned when later exported into PDF.
                     The direct link to the visa application, it should be government-related official website like this: https://mzv.gov.cz/jnp/en/information_for_aliens/visa_form/index.html instead of some general website link.
                     Do this too to the other links as well.
-                    For the last page of Farewell Messages, no <h1> title needed, no <p></p> needed, just wrap all the farewell messages sentences into one <h2 id='conclusion'></h2>, you can try <br> to break lines between sentences if you have to.
+                    For the last page of Farewell Messages, no <h1> title needed, no <p></p> needed, just wrap all the farewell messages sentences into one <h2 id='conclusion'></h2>, you can use <br> to break lines between sentences if you have to.
                     
                     Response Tone:
                     Remember not to provide the response in a dialogue or conversation form, instead you should reply in an informative and descriptive way.
