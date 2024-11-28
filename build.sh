b8d51a1335d11724c56d74bb3eeeb69984d32bb8
Terrance <terrancehah@gmail.com>
2024-11-29 05:07:47 +0800

    public file ignore

--- build.sh
+++ build.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# Ensure dist directory exists
-mkdir -p dist
-
-# Build Tailwind CSS with PostCSS
+# Build Tailwind CSS
 npx tailwindcss -i ./tailwind.css -o ./dist/output.css --minify
-
-# Copy necessary files to public directory
-mkdir -p public
-cp -r dist public/
-cp -r resources public/
-cp *.html public/
-cp *.js public/
-cp *.css public/
